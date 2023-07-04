622317431708e14c175e6d818c8ae4963b730a8a
Iris <i5052637@gmail.com>
2023-07-04 20:54:47 +0800

    add unit test

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,5 +1,5 @@
 
-from engine import Engine
+from engine.engine import Engine
 
 
 class WilloughbyEngine(Engine):
@@ -7,5 +7,5 @@ class WilloughbyEngine(Engine):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
         return self.current_mileage - self.last_service_mileage > 60000
